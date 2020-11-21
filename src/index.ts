a8112cf9624530d76a83d5bfe0dc3dc8e0024dfd
qianbin <cola.tin.com@gmail.com>
2020-11-21 17:35:34 +0800

    support the flag 'appTriggered'

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,7 @@ export interface Stack {
     readonly canGoBack: boolean
     readonly full: Entry[]
     readonly scoped: ScopedEntry[]
+    readonly appTriggered: boolean
 }
 
 function getScopeRoot(vm: Vue, entries: Entry[]): [RouteRecord, number] | undefined {
@@ -41,6 +42,7 @@ export default function install(Vue: typeof _Vue, options?: Options) {
 
     const router = options.router
     const stack = Vue.observable<{ entries: Entry[] }>({ entries: [] })
+    let appTriggerN = 0
 
     Object.defineProperty(Vue.prototype, '$stack', {
         get(): Stack {
@@ -83,6 +85,9 @@ export default function install(Vue: typeof _Vue, options?: Options) {
                                 get component() { return rec.components.default as Vue.Component }
                             }
                         })
+                },
+                get appTriggered() {
+                    return appTriggerN > 0
                 }
             }
         }
@@ -92,8 +97,10 @@ export default function install(Vue: typeof _Vue, options?: Options) {
     let depth = (history.state || {}).__depth || 0
     const pushStateFn = history.pushState.bind(history)
     const replaceStateFn = history.replaceState.bind(history)
+    const goFn = history.go.bind(history)
 
     history.pushState = (data, title, url) => {
+        appTriggerN = 1
         data = data || {}
         depth++
         pushStateFn({
@@ -102,14 +109,20 @@ export default function install(Vue: typeof _Vue, options?: Options) {
         }, title, url)
     }
     history.replaceState = (data, title, url) => {
+        appTriggerN = 1
         data = data || {}
         replaceStateFn({
             __depth: depth,
             ...data
         }, title, url)
     }
+    history.go = (delta) => {
+        appTriggerN = 2
+        goFn(delta)
+    }
     // for the case manually input url to navigate
     window.addEventListener('popstate', () => {
+        appTriggerN--
         const data = history.state || {}
         if (typeof data.__depth === 'number') {
             depth = data.__depth
